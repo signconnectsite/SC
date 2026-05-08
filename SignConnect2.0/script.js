fa6940e1d4232754d8618956226f77875a3e2fdc
signconnectsite <signconnect.site@gmail.com>
2026-05-07 21:35:43 -0300

    V2.0
    -Estilização para aproximar a estrutura do site ao planejamento de design

---
+++ SignConnect2.0/script.js
@@ -0,0 +1,14 @@
+let lastScrollTop = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop && scrollTop > 100) {
+        header.classList.add('header-hidden');
+    } else {
+        header.classList.remove('header-hidden');
+    }
+    
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; 
+}, false);
